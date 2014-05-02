0ad859b16bb8e40f4f87a740ec3460afe462b148
kyung yeol kim <chitacan@gmail.com>
2014-05-02 14:53:44 +0900

    add setup script.

---
+++ files/setup.sh
@@ -0,0 +1,5 @@
+cd /home/vagrant
+git clone https://github.com/chitacan/huraemon
+git clone ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_me
+git clone ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_plus20
+git clone ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_aos
