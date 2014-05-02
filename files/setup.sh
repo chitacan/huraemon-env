971760e41df3c97fac3c1385cef9eeae40760770
kyung yeol kim <chitacan@gmail.com>
2014-05-02 18:30:56 +0900

    install frontend module in setup process.

--- files/setup.sh
+++ files/setup.sh
@@ -1,5 +1,6 @@
 cd /home/vagrant
 cd ~ && git clone https://github.com/chitacan/huraemon && cd huraemon && npm install --production
+bower install
 npm install git+ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_me
 npm install git+ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_plus20
 npm install git+ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_aos
