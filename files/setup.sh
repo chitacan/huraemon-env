fd8b787e907bfc3f352de5795d52760def46f0f3
kyung yeol kim <chitacan@gmail.com>
2014-05-02 18:03:10 +0900

    update setup script.

--- files/setup.sh
+++ files/setup.sh
@@ -1,5 +1,5 @@
 cd /home/vagrant
-git clone https://github.com/chitacan/huraemon
-git clone ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_me
-git clone ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_plus20
-git clone ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_aos
+cd ~ && git clone https://github.com/chitacan/huraemon && cd huraemon && npm install --production
+npm install git+ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_me
+npm install git+ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_plus20
+npm install git+ssh://pi@chitacan.dlinkddns.com:8888/home/pi/BTSync/git/pp_aos
